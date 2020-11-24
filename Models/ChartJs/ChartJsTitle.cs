e5041aba0c1397d1b0b3177469286df9d68e7b7e
ricban <ricky.bancolita@gmail.com>
2020-11-24 11:09:44 -0500

    Initial Commit

---
+++ Models/ChartJs/ChartJsTitle.cs
@@ -0,0 +1,9 @@
+﻿namespace Covid19.Client.Models
+{
+    public class ChartJsTitle
+    {
+        public bool Display { get; set; } = default!;
+        public string Text { get; set; } = default!;
+        public string Position { get; set; } = default!;
+    }
+}
